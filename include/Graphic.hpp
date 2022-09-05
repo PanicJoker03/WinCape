da96c6b2d88430d6fd2c7e44ff005dab4052f101
unknown <panicjoker03@outlook.com>
2022-09-05 00:30:27 -0500

    Graphic namespace idea

--- include/Graphic.hpp
+++ include/Graphic.hpp
@@ -2,6 +2,17 @@
 #define GRPAHIC_HPP
 namespace cap {
 namespace ghc {
+    namespace gdi {
 
+    }
+    namespace d2d {
+
+    }
+    namespace ogl {
+        
+    }
+    namespace drx {
+        
+    }
 }}
 #endif
