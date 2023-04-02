3df4881ec667a2c020812a2229b7458708e5ab86
Luis Núñez <panicjoker03@outlook.com>
2023-04-02 00:22:26 -0600

    Get/Set custom properties

--- include/Ui/Base.h
+++ include/Ui/Base.h
@@ -10,6 +10,12 @@ namespace ui{
 	public:
 		void setText(WSTR_CON text);
 		void getText(PTR_WSTR text, int lenght) const;
+		void setProp(WCH_STR_CON prop, long val);
+		void setProp(WCH_STR_CON prop, int val);
+		void setProp(WCH_STR_CON prop, float val);
+		//void setProp(WCH_STR_CON prop, double val);
+		template<typename T>
+		T getProp(WCH_STR_CON prop);
 	protected:
 		Base();
 	};
